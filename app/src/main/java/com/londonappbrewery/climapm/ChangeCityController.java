e81a1742812091b2183b96ea9f295ea23591bad3
Blanco <32441027+sirBlanco@users.noreply.github.com>
2019-10-03 22:24:14 -0400

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -0,0 +1,45 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Link Changecity controller to the ChangeCity layout file
+        setContentView(R.layout.change_city_layout);
+        final EditText editTextField = findViewById(R.id.queryET);
+        ImageButton backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //Allows the app to get the city name enter by the user and pass it to the
+        //Weather controller
+        editTextField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String newCity = editTextField.getText().toString();
+                Intent newCityIntent = new Intent(ChangeCityController.this, WeatherController.class);
+                newCityIntent.putExtra("City", newCity);
+                startActivity(newCityIntent);
+
+                return false;
+            }
+        });
+
+    }
+}
